1fde15b80d2c78b99016bf20ed35a07f38247816
Benjamin RAUDIN <benjamin.raudin@socgen.com>
2025-02-28 14:34:35 +0100

    fix(2015): day 12

--- src/2015/day-12/index.ts
+++ src/2015/day-12/index.ts
@@ -4,6 +4,5 @@ import { day12p1 } from './part1';
 import { day12p2 } from './part2';
 
 const input = getInput(resolve(__dirname, 'data.txt'));
-
 console.info('2015 day12 p1', day12p1(input));
 console.info('2015 day12 p2', day12p2(input));
